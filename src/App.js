35c9ac9d012be48fc8657ace31f6e87273ef64e8
Rubens Kleinschmidt Júnior <36218162+RubensKj@users.noreply.github.com>
2020-03-28 22:47:47 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,139 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+import MicrophoneStream from 'microphone-stream';
+import getUserMedia from 'get-user-media-promise';
+
+import './styles/styles.css';
+import './styles/global.css';
+
+const OPTIONS = {
+  stream: null,
+  objectMode: true,
+  bufferSize: null,
+  context: null
+}
 
 function App() {
+
+  // Audio Settings
+  const [micStream, setMicStream] = useState(new MicrophoneStream(OPTIONS));
+  const [audioContext] = useState(new AudioContext());
+  const [audioBuffer, setAudioBuffer] = useState({});
+
+  // Variables Settings
+  const [isCapturing, setIsCapture] = useState(false);
+  const [isToListen, setIsToListen] = useState(false);
+
+  // Socket Settings
+  const [socketIo, setSocketIo] = useState({});
+
+
+  useEffect(() => {
+    const socket = io('http://192.168.25.17:3333', {
+      query: { user: '123' }
+    });
+
+
+    setSocketIo(socket);
+
+    socket.on('listen', raw => {
+      async function putToListen(raw) {
+        if (raw) {
+          try {
+            let source = audioContext.createBufferSource();
+            source.buffer = audioContext.createBuffer(raw.numberOfChannels, raw.length, raw.sampleRate);
+            source.connect(audioContext.destination);
+
+            // play audio
+            source.start();
+          } catch (error) {
+            console.log(error)
+            return;
+          }
+        }
+      }
+
+      putToListen(raw);
+    });
+  }, [audioContext]);
+
+  function startCapturing() {
+    setIsCapture(!isCapturing);
+
+    setMicStream(new MicrophoneStream(OPTIONS));
+
+    getUserMedia({ video: false, audio: true })
+      .then(function (stream) {
+        micStream.setStream(stream);
+      }).catch(function (error) {
+        console.log(error);
+      });
+
+    micStream.on('data', function (chunk) {
+      setAudioBuffer(chunk);
+      sendVoiceToServer(chunk, socketIo);
+    });
+
+    micStream.on('format', function (format) {
+      console.log(format);
+    });
+  }
+
+  function sendVoiceToServer(raw, socketConnection) {
+    socketConnection.emit('voice', {
+      length: raw.length,
+      duration: raw.duration,
+      sampleRate: raw.sampleRate,
+      numberOfChannels: raw.numberOfChannels,
+    });
+  }
+
+  useEffect(() => {
+    if (isToListen) {
+      try {
+        let source = audioContext.createBufferSource();
+        source.buffer = audioBuffer;
+        source.connect(audioContext.destination);
+
+        // play audio
+        source.start();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, [audioBuffer, audioContext, isToListen]);
+
+  function listenYourVoice() {
+    setIsToListen(!isToListen);
+  }
+
+  function stopCapturing() {
+    setIsCapture(false);
+    setIsToListen(false);
+    micStream.stop();
+    setMicStream(new MicrophoneStream(OPTIONS));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        {isCapturing === false ? (
+          <button onClick={() => startCapturing(socketIo)}>Start capture your voice</button>
+        ) : (
+            <button onClick={stopCapturing}>Stop capturing your voice</button>
+          )}
+        {isCapturing === true ? (
+          <>
+            {isToListen === true ? (
+              <button onClick={() => listenYourVoice()}>Stop listening your voice</button>
+            ) : (
+                <button onClick={() => listenYourVoice()}>Listen your voice</button>
+              )}
+          </>
+        ) : (
+            <button disabled>Listen your voice</button>
+          )}
+      </div>
     </div>
   );
 }
